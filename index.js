338ecf3c319b1c470900b8676befb459c50d62f1
FKai96 <153430149+FKai96@users.noreply.github.com>
2024-03-22 22:18:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random;
+random = 1;
+Math.random(random);
+
+let mod;
+let num3 = 24;
+let num4 = 5;
+mod = num3 % num4;
+
+let max;
+max = Math.max(5, 10, 15, 20);
